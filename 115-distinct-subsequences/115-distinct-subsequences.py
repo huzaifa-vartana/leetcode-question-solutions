1e406da439cdb6eb386dd42a1406d0bbcb4d6e71
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-09-20 23:43:35 +0500

    Time: 1417 ms (29.02%), Space: 71.3 MB (50.34%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.py
+++ 115-distinct-subsequences/115-distinct-subsequences.py
@@ -1,19 +1,18 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
+        # if idx1 >= n: return 1 if idx2 >= m else 0
+        # if cache[idx1][idx2] != -1: return cache[idx1][idx2]
         
         n, m = len(s), len(t)
-        cache = [[-1 for _ in range(m+1)] for _ in range(n+1)]
-        def solve(idx1, idx2):
+        cache = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        for row in range(n+1): cache[row][m] = 1
+        for idx1 in range(n-1, -1, -1):
+            for idx2 in range(m-1, -1, -1):
+                a = 0
+                if idx1 < n and idx2 < m and s[idx1] == t[idx2]: a = cache[idx1+1][idx2+1]
+
+                cache[idx1][idx2] = a + cache[idx1+1][idx2]
             
-            if idx1 >= n: return 1 if idx2 >= m else 0
-            if cache[idx1][idx2] != -1: return cache[idx1][idx2]
-            
-            a = 0
-            if idx1 < n and idx2 < m and s[idx1] == t[idx2]: a = solve(idx1+1, idx2+1)
-            
-            cache[idx1][idx2] = a + solve(idx1+1, idx2)
-            return cache[idx1][idx2] 
-            
-        return solve(0, 0)
+        return cache[0][0]
         
