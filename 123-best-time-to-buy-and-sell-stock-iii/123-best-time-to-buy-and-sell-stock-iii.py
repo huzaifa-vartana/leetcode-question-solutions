b73a42c7e2d719ad45bdaeea7f8e62cda6b47dc5
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-09-24 20:47:45 +0500

    Time: 9127 ms (5.01%), Space: 147.4 MB (12.89%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -2,30 +2,54 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
         n = len(prices)
+        cache = [[-1 for _ in range(4)] for _ in range(n+1)]
+        def solve(idx, trans):
+            if idx >= n: return 0
+            if trans >= 4: return 0
+            if cache[idx][trans] != -1: return cache[idx][trans]
+            
+            buy = trans % 2 == 0
 
-        # cache = [[[0 for _ in range(3)] for _ in range(2)] for _ in range(n+1)]
-        prev = [[0 for _ in range(3)] for _ in range(2)]
+            if buy:
+                buy = -prices[idx] + solve(idx+1, trans+1)
+                notBuy = solve(idx+1, trans)
+                
+                cache[idx][trans] = max(buy, notBuy)
+                return cache[idx][trans]
+            else:
+                cache[idx][trans] = max(
+                    prices[idx] + solve(idx+1, trans+1),
+                    solve(idx+1, trans)
+                )
+                return cache[idx][trans]
+                
         
         
-        for idx in range(n-1, -1, -1):
-            curr = [[0 for _ in range(3)] for _ in range(2)]
-            for lastBought in [True, False]:
-                for totalSales in range(2):
-                    if not lastBought:
-                        buy = -prices[idx] + prev[True][totalSales]
-                        notBuy = prev[False][totalSales]
+        return solve(0, 0)
 
-                        curr[lastBought][totalSales] = max(buy, notBuy) 
+#         # cache = [[[0 for _ in range(3)] for _ in range(2)] for _ in range(n+1)]
+#         prev = [[0 for _ in range(3)] for _ in range(2)]
+        
+        
+#         for idx in range(n-1, -1, -1):
+#             curr = [[0 for _ in range(3)] for _ in range(2)]
+#             for lastBought in [True, False]:
+#                 for totalSales in range(2):
+#                     if not lastBought:
+#                         buy = -prices[idx] + prev[True][totalSales]
+#                         notBuy = prev[False][totalSales]
 
-                    elif lastBought:
-                        curr[lastBought][totalSales] = max(
-                            prices[idx] + prev[False][totalSales+1],
-                            prev[lastBought][totalSales])
+#                         curr[lastBought][totalSales] = max(buy, notBuy) 
 
-            prev = curr.copy()
+#                     elif lastBought:
+#                         curr[lastBought][totalSales] = max(
+#                             prices[idx] + prev[False][totalSales+1],
+#                             prev[lastBought][totalSales])
+
+#             prev = curr.copy()
 
         
-        return prev[False][0]
+#         return prev[False][0]
                     
                     
                     
