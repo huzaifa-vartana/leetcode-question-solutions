cfdc0100e78c30ac0ad7f135a43499b41f361141
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-09-24 16:34:11 +0500

    Time: 138 ms (22.15%), Space: 15.2 MB (69.25%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -2,14 +2,12 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
         n = len(prices)
-
-
-        # cache = [[float('-inf') for _ in range(2)] for _ in range(n+1)]
-        # cache[n][True] = 0
-        # cache[n][False] = 0
-        curr, prev = [float('-inf')] * (2), [float('-inf')] * (2)
+        
+        prev = [float('-inf')] * (2)
         prev[False] = prev[True] = 0
+        
         for idx in range(n-1, -1, -1):
+            curr = [float('-inf')] * (2)
             for lastBought in [True, False]:
 
                 if not lastBought:
