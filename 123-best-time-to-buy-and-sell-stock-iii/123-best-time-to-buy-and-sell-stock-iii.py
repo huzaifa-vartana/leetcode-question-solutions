3d5e7ae84e198af1901e0f6a23160f18eb7f26af
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-09-24 17:32:00 +0500

    Time: 7763 ms (5.01%), Space: 55.6 MB (20.92%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -3,27 +3,49 @@ class Solution:
         
         n = len(prices)
 
-        cache = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(n+1)]
-        def solve(idx, lastBought, totalSales):
+        cache = [[[0 for _ in range(3)] for _ in range(2)] for _ in range(n+1)]
+        for idx in range(n-1, -1, -1):
+            for lastBought in [True, False]:
+                for totalSales in range(2):
+                    if not lastBought:
+                        buy = -prices[idx] + cache[idx+1][True][totalSales]
+                        notBuy = cache[idx+1][False][totalSales]
 
-            if idx >= n: return 0
-            if totalSales >= 2: return 0
-            if cache[idx][lastBought][totalSales] != -1: return cache[idx][lastBought][totalSales]
+                        cache[idx][lastBought][totalSales] = max(buy, notBuy) 
 
-            if not lastBought:
-                buy = -prices[idx] + solve(idx+1, True, totalSales)
-                notBuy = solve(idx+1, False, totalSales)
-                
-                cache[idx][lastBought][totalSales] = max(buy, notBuy)
-                return cache[idx][lastBought][totalSales] 
-            
-            elif lastBought:
-                cache[idx][lastBought][totalSales] = max(
-                    prices[idx] + solve(idx+1, False, totalSales + 1),
-                    solve(idx+1, lastBought, totalSales)
-                )
-                return cache[idx][lastBought][totalSales]
+                    elif lastBought:
+                        cache[idx][lastBought][totalSales] = max(
+                            prices[idx] + cache[idx+1][False][totalSales+1],
+                            cache[idx+1][lastBought][totalSales])
 
-            return float('-inf')
+
+
         
-        return solve(0, False, 0)
+        return cache[0][False][0]
+                    
+                    
+                    
+        
+#         def solve(idx, lastBought, totalSales):
+
+#             if idx >= n: return 0
+#             if totalSales >= 2: return 0
+#             if cache[idx][lastBought][totalSales] != -1: return cache[idx][lastBought][totalSales]
+
+#             if not lastBought:
+#                 buy = -prices[idx] + solve(idx+1, True, totalSales)
+#                 notBuy = solve(idx+1, False, totalSales)
+                
+#                 cache[idx][lastBought][totalSales] = max(buy, notBuy)
+#                 return cache[idx][lastBought][totalSales] 
+            
+#             elif lastBought:
+#                 cache[idx][lastBought][totalSales] = max(
+#                     prices[idx] + solve(idx+1, False, totalSales + 1),
+#                     solve(idx+1, lastBought, totalSales)
+#                 )
+#                 return cache[idx][lastBought][totalSales]
+
+#             return float('-inf')
+        
+#         return solve(0, False, 0)
