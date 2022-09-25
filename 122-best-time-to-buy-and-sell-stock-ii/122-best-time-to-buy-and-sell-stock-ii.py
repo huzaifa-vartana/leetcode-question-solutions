49bb1c2854797d76506ee196bf7d18fd85b29bfc
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-09-25 23:49:44 +0500

    Time: 192 ms (5.01%), Space: 17.5 MB (5.93%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -3,19 +3,13 @@ class Solution:
         
         
         n = len(prices)
-        cache = [[-1 for _ in range(2)] for _ in range(n+1)]
-        
-        
-        
-        def solve(idx, buy):
-            if idx >= n: return 0
-            if cache[idx][buy] != -1: return cache[idx][buy]
+        cache = [[0 for _ in range(2)] for _ in range(n+1)]
+        for idx in range(n-1, -1, -1):
+            for buy in [True, False]:
+
+                if buy:
+                    cache[idx][buy] = max(-prices[idx] + cache[idx+1][False], cache[idx+1][True])
+                else:
+                    cache[idx][buy] = max(prices[idx] + cache[idx+1][True], cache[idx+1][buy])
             
-            if buy:
-                cache[idx][buy] = max(-prices[idx] + solve(idx+1, False), solve(idx+1, True))
-                return cache[idx][buy]
-            else:
-                cache[idx][buy] = max(prices[idx] + solve(idx+1, True), solve(idx+1, buy))
-                return cache[idx][buy]
-            
-        return solve(0, True)
+        return cache[0][True]
