43b6f7fd8e1e70b7d7a3d3b4fb297de361cc30f8
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-09-25 18:48:17 +0500

    Time: 247 ms (53.99%), Space: 16.5 MB (87.21%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -2,34 +2,27 @@ class Solution:
     def minDistance(self, s: str, t: str) -> int:
         
         
-        l1, l2 = len(s), len(t)
-
-        cache = [[float('inf') for _ in range(l2+1)] for _ in range(l1+1)]
-        cache[l1][l2] = 0
-        for i in range(l2): cache[l1][i] = l2 - i
-        for j in range(l1): cache[j][l2] = l1 - j
-        for i in range(l1-1, -1, -1):
-            for j in range(l2-1, -1, -1):
-
-                count = float('inf')
-                if s[i] == t[j]: count = min(count, cache[i+1][j+1])
-                else:
-                    replace = 1 + cache[i+1][j+1]
-                    delete = 1 + cache[i+1][j]
-                    insert = 1 + cache[i][j+1]
-
-                    count = min(delete, insert, replace)
-
-
-                cache[i][j] = count
-
-
-
-        return cache[0][0]
-    
-    
-#             def solve(i, j):
-
-#             if i >= l1 and j >= l2: return 0
-#             if i >= l1: return l2 - j
-#             if j >= l2: return l1 - i
+        
+        n, m = len(s), len(t)
+        cache = [[-1 for _ in range(m+1)] for _ in range(n+1)]
+        def cache1(i, j):
+            
+            if i >= n and j >= m: return 0
+            if i >= n: return m - j
+            if j >= m: return n - i
+            if cache[i][j] != -1: return cache[i][j]
+            
+            
+            
+            if s[i] != t[j]:
+                cache[i][j] = 1 + min(cache1(i+1, j+1), cache1(i, j+1), cache1(i+1, j))
+                return cache[i][j]
+            
+            cache[i][j] = cache1(i+1, j+1)
+            return cache[i][j]
+            
+                
+        return cache1(0, 0)
+        
+        
+        
