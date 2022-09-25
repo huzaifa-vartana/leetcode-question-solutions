bd3b7092e77b7baa5e72c7fe13f752f2a754e0da
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-09-26 00:44:50 +0500

    Time: 83 ms (40.38%), Space: 14.6 MB (52.05%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -3,21 +3,21 @@ class Solution:
         
         
         n = len(prices)
-        cache = [[-1 for _ in range(2)] for _ in range(n+1)]
-        
-        
-        def solve(idx, buy):
+        cache = [[0 for _ in range(2)] for _ in range(n+1)]
+        for idx in range(n-1, -1, -1):
+            for buy in [True, False]:
+                
+                if buy:
+                    cache[idx][buy] = max(-prices[idx] + cache[idx+1][False], cache[idx+1][True])
+
+                else:
+                    if idx+2 <= n: 
+                        tmp = prices[idx] + cache[idx+2][True]
+                    else:
+                        tmp = prices[idx] + 0
+                    cache[idx][buy] = max(tmp, cache[idx+1][buy])
+
             
-            if idx >= n: return 0
-            if cache[idx][buy] != -1: return cache[idx][buy]
-            
-            if buy:
-                cache[idx][buy] = max(-prices[idx] + solve(idx+1, False), solve(idx+1, True))
-                return cache[idx][buy]
-            else:
-                cache[idx][buy] = max(prices[idx] + solve(idx+2, True), solve(idx+1, buy))
-                return cache[idx][buy]
             
             
-            
-        return solve(0, True)
+        return cache[0][True]
