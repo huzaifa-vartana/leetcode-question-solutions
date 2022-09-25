664ed04068d5baae27a75a0de473bb429437a26f
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-09-25 18:30:03 +0500

    Time: 1801 ms (17.33%), Space: 71.3 MB (50.96%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.py
+++ 115-distinct-subsequences/115-distinct-subsequences.py
@@ -4,23 +4,19 @@ class Solution:
         n, m = len(s), len(t)
         
         
-        cache = [[-1 for _ in range(m+1)] for _ in range(n+1)]
-        def solve(i, j):
-            
-            if j >= m: return 1
-            if i >= n: return 0
-            if i >= n: return 1 if j >= m else 0
-            if cache[i][j] != -1: return cache[i][j]
-            
-            count = 0
-            if s[i] == t[j]:
-                count += solve(i+1, j+1)
+        cache = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        for row in range(n+1): cache[row][m] = 1
+        
+        for i in range(n-1, -1, -1):
+            for j in range(m-1, -1, -1):
                 
-            cache[i][j] = count + solve(i+1, j)
-            return cache[i][j] 
+                count = 0
+                if s[i] == t[j]: count += cache[i+1][j+1]
+                cache[i][j] = count + cache[i+1][j]
+
         
         
-        return solve(0, 0)
+        return cache[0][0]
         
         
         
