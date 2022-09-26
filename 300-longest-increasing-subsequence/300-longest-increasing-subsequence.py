1289a4f18e1d065dff86849ff2ccda188f1f7ad0
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-09-27 02:08:28 +0500

    Time: 215 ms (65.95%), Space: 14.3 MB (13.21%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,15 +1,48 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
-        n = len(nums)
-        cache = [1] * (n+1)
+        def bs(arr, t):
+            left, right = 0, len(arr) - 1
+            while left <= right:
+                mid = (left+right) // 2
+                if arr[mid] == t: 
+                    return mid
+                elif arr[mid] < t:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+                    
+            return left
         
-        lis = 0
-        for idx in range(n):
-            for prevIdx in range(idx):
+        n = len(nums)
+        lis = [nums[0]]
+        for idx in range(1, n):
+            if nums[idx] > lis[-1]:
+                lis.append(nums[idx])
+            else:
+                tmp = bs(lis, nums[idx])
+                lis[tmp] = nums[idx] 
+        
+        return len(lis)
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         n = len(nums)
+#         cache = [1] * (n+1)
+        
+#         lis = 0
+#         for idx in range(n):
+#             for prevIdx in range(idx):
                 
-                if nums[idx] > nums[prevIdx]: cache[idx] = max(cache[idx], 1 + cache[prevIdx])
+#                 if nums[idx] > nums[prevIdx]: cache[idx] = max(cache[idx], 1 + cache[prevIdx])
                 
-            lis = max(lis, cache[idx])
+#             lis = max(lis, cache[idx])
                 
-        return lis
+#         return lis
