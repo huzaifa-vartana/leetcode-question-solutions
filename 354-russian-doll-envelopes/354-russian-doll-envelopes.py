bdda2dd52afaf05a20f4ebe5c4b9f3c5e0b21eeb
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-10 01:58:57 +0500

    Time: 1345 ms (88.13%), Space: 61.8 MB (27.77%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.py
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.py
@@ -1,30 +1,23 @@
 class Solution:
-    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+    def maxEnvelopes(self, nums: List[List[int]]) -> int:
         
-        envelopes.sort(key = lambda x: [x[0], -x[1]])
-        n = len(envelopes)
-
-        def bs(nums, key):
-            low, high = 0, len(nums) - 1
-            while low <= high:
-                mid = (low+high) // 2
-                if nums[mid] == key:
-                    return mid
-                if nums[mid] > key:
-                    high = mid - 1
-                else:
-                    low = mid + 1
-
-            return low
+        n = len(nums)
+        nums.sort(key = lambda x: [x[0], -x[1]])
+        print(nums)
         
-        
-        res = [envelopes[0][1]]
+        def bs(key, arr):
+            l, h = 0, len(arr) - 1
+            while l <= h:
+                mid = (l+h) // 2
+                if arr[mid] == key: return mid
+                elif arr[mid] > key: h = mid - 1
+                else: l = mid + 1
+            return l
+            
+            
+        res = [nums[0][1]]
         for idx in range(1, n):
-            if envelopes[idx][1] > res[-1]:
-                res.append(envelopes[idx][1])
-            else:
-                res[bs(res, envelopes[idx][1])] = envelopes[idx][1]
-                
+            if nums[idx][1] > res[-1]: res.append(nums[idx][1])
+            else: res[bs(nums[idx][1], res)] = nums[idx][1]
+            
         return len(res)
-        
-        
