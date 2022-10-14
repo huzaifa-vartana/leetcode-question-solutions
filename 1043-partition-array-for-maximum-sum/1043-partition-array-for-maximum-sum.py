8ce89fe6306bdc55ecb96c863bea0dfd78e659fe
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-15 02:33:43 +0500

    Time: 7010 ms (5.14%), Space: 13.9 MB (72.39%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -2,22 +2,16 @@ class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], part: int) -> int:
         
         n = len(arr)
-        cache = [-1 for _ in range(n+1)]
+        cache = [0 for _ in range(n+1)]
+        for idx in range(n-1, -1, -1):
         
-        
-        def partition(idx):
-
-            if idx >= n: return 0
-            if cache[idx] != -1: return cache[idx]
-
             max_sum = 0
             itr = min(idx + part, n)
             for k in range(idx, itr):
 
                 subarray_sum = max(arr[idx:k+1]) * (k - idx + 1)
-                max_sum = max(max_sum, subarray_sum + partition(k + 1))
+                max_sum = max(max_sum, subarray_sum + cache[k+1])
 
             cache[idx] = max_sum
-            return cache[idx] 
         
-        return partition(0)
+        return cache[0]
