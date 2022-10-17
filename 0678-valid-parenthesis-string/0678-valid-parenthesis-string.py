7fbbfc3b4773f12bfeed17e818916dccc1e163c1
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-17 20:07:07 +0500

    Time: 130 ms (5.23%), Space: 13.9 MB (28.74%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -2,19 +2,19 @@ class Solution:
     def checkValidString(self, s: str) -> bool:
         
         n = len(s)
-        cache = [[False for _ in range(n+1)] for _ in range(n+1)]
-        cache[n][0] = True
+        prev = [False for _ in range(n+1)]
+        prev[0] = True
         
         for idx in range(n-1, -1, -1):
+            curr = [False for _ in range(n+1)]
             for open_paran in range(n-1, -1, -1):
-                
                 if s[idx] == '(':
-                    cache[idx][open_paran] = cache[idx+1][open_paran+1] 
+                    curr[open_paran] = prev[open_paran+1] 
                 elif s[idx] == ')':
-                    cache[idx][open_paran] = cache[idx+1][open_paran-1]
+                    curr[open_paran] = prev[open_paran-1]
                 else:
-                    cache[idx][open_paran] = cache[idx+1][open_paran+1] or cache[idx+1][open_paran-1] or cache[idx+1][open_paran] 
+                    curr[open_paran] = prev[open_paran+1] or prev[open_paran-1] or prev[open_paran] 
 
-
+            prev = curr
         
-        return cache[0][0]
+        return prev[0]
