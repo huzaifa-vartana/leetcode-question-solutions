28265114ec3bb272f214148322ead6347786e07d
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-17 22:04:16 +0500

    Time: 1151 ms (86.90%), Space: 14.2 MB (74.66%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -5,24 +5,19 @@ class Solution:
         cuts.append(n)
         cuts.sort()
         n = len(cuts)
-        cache = [[-1 for _ in range(n+1)] for _ in range(n+1)]
+        cache = [[0 for _ in range(n+1)] for _ in range(n+1)]
         
-        @lru_cache(None)
-        def solve(i, j):
+        for i in range(n, -1, -1):
+            for j in range(i, n-1):
+                
+                mini = float('inf')
+                for k in range(i, j+1):
 
-            if i > j: return 0
-            # if cache[i][j] != -1: return cache[i][j]
-            
-            mini = float('inf')
-            for k in range(i, j+1):
+                    mini = min(
+                        mini,
+                        cuts[j+1] - cuts[i-1] + cache[i][k-1] + cache[k+1][j]
+                    )
+                cache[i][j] = mini
 
-                mini = min(
-                    mini,
-                    cuts[j+1] - cuts[i-1] + solve(i, k-1) + solve(k+1, j)
-                )
-            # cache[i][j] = mini
-            # return cache[i][j]
-            return mini
 
-        
-        return solve(1, n-2)
+        return cache[1][n-2]
