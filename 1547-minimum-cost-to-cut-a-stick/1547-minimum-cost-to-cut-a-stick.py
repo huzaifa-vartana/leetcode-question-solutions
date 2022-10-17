7d86ff961904bfd3a9cd10c3be7f4a85a88b3f3b
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-17 21:51:12 +0500

    Time: 3241 ms (34.66%), Space: 17.7 MB (40.86%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -7,10 +7,11 @@ class Solution:
         n = len(cuts)
         cache = [[-1 for _ in range(n+1)] for _ in range(n+1)]
         
+        @lru_cache(None)
         def solve(i, j):
 
             if i > j: return 0
-            if cache[i][j] != -1: return cache[i][j]
+            # if cache[i][j] != -1: return cache[i][j]
             
             mini = float('inf')
             for k in range(i, j+1):
@@ -19,9 +20,9 @@ class Solution:
                     mini,
                     cuts[j+1] - cuts[i-1] + solve(i, k-1) + solve(k+1, j)
                 )
-            cache[i][j] = mini
-            return cache[i][j]
+            # cache[i][j] = mini
+            # return cache[i][j]
+            return mini
 
-        
         
         return solve(1, n-2)
