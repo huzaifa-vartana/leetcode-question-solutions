7a19ca7929be783574eee6c43f0d0c950e30a98a
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-17 22:23:45 +0500

    Time: 410 ms (22.30%), Space: 17.4 MB (68.15%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
@@ -9,18 +9,20 @@ class Solution:
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 
-        @lru_cache(None)
+        cache = [[[-1 for _ in range(maxMove+1)] for _ in range(n+1)] for _ in range(m+1)]
         def solve(i, j, moves):
 
             if not valid(i, j): return 1
             if moves <= 0: return 0
+            if cache[i][j][moves] != -1: return cache[i][j][moves]
 
             count = 0
             for dx, dy in directions:
                 nr, nc = dx + i, dy + j
                 count += solve(nr, nc, moves - 1)
 
-            return count % MOD
+            cache[i][j][moves] = count % MOD
+            return cache[i][j][moves] 
 
 
         return solve(startRow, startColumn, maxMove)
