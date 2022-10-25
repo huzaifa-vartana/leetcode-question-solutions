ccb2dba501a574ea6da5f05ce1d0fa5675450962
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-25 23:14:04 +0500

    Time: 1006 ms (51.60%), Space: 22.8 MB (41.15%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,19 +2,16 @@ class Solution:
     def longestCommonSubsequence(self, t1: str, t2: str) -> int:
         
         m, n = len(t1), len(t2)
-        cache = [[-1 for _ in range(n + 1)] for _ in range(m + 1)]
+        cache = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
         
-        def solve(i, j):
-            if i >= m or j >= n: return 0
-            if cache[i][j] != -1: return cache[i][j]
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
             
-            if t1[i] == t2[j]:
-                cache[i][j] = 1 + solve(i+1, j+1)
-                return cache[i][j]
-            cache[i][j] = max(solve(i+1, j), solve(i, j+1))
-            return cache[i][j]
+                if t1[i] == t2[j]: cache[i][j] = 1 + cache[i+1][j+1]
+                else: cache[i][j] = max(cache[i][j+1], cache[i+1][j])
+
         
         
 
-        return solve(0, 0)
+        return cache[0][0]
         
