9de456d4045ec7a14f0a112050ee3f1114833267
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-25 21:37:56 +0500

    Time: 5223 ms (5.17%), Space: 188.8 MB (6.03%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
@@ -3,20 +3,28 @@ class Solution:
         n1 = len(nums1)
         n2 = len(nums2)
 
-        cache = [[[-1 for _ in range(2)] for _ in range(n2+1)] for _ in range(n1+1)]
+        # cache = [[[-1 for _ in range(2)] for _ in range(n2+1)] for _ in range(n1+1)]
+        cache = {}
         def solve(i, j, flag):
+            state = (i, j, flag)
             if i == n1 or j == n2:
-                cache[i][j][flag] = float('-inf') if not flag else 0
-                return cache[i][j][flag]
-            if cache[i][j][flag] != -1: return cache[i][j][flag]
+                # cache[i][j][flag] = float('-inf') if not flag else 0
+                cache[state] = float('-inf') if not flag else 0
+                # return cache[i][j][flag]
+                return cache[state]
+            # if cache[i][j][flag] != -1: return cache[i][j][flag]
+            if state in cache: return cache[state]
 
             a = solve(i+1, j, flag)
             b = solve(i, j+1, flag)
             c = nums1[i] * nums2[j] + solve(i+1, j+1, True)
             d = solve(i+1, j+1, flag)
 
-            cache[i][j][flag] = max(a, b, c, d)
-            return cache[i][j][flag]
+            # cache[i][j][flag] = max(a, b, c, d)
+            # return cache[i][j][flag]
+            cache[state] = max(a, b, c, d)
+            return cache[state]
+            
         
         
         
