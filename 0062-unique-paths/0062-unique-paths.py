1ddd441ca7a4c65aab4acbda44007f13ca957567
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-25 23:04:04 +0500

    Time: 32 ms (94.22%), Space: 13.8 MB (74.19%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -2,19 +2,20 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
         def valid(x, y): return x >= 0 and x < m and y >= 0 and y < n
-        cache = [[-1 for _ in range(n + 1)] for _ in range(m + 1)]
-        
-        def solve(i, j):
-            if i == m - 1 and j == n - 1: return 1
-            if cache[i][j] != -1: return cache[i][j]
+        cache = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        cache[m-1][n-1] = 1
+
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if i == m - 1 and j == n - 1: continue
+    
+                count = 0
+                for nr, nc in [(i+1, j), (i, j+1)]:
+                    if not valid(nr, nc): continue
+                    count += cache[nr][nc]
+
+                cache[i][j] = count
+
             
-            count = 0
-            for nr, nc in [(i+1, j), (i, j+1)]:
-                if not valid(nr, nc): continue
-                count += solve(nr, nc)
-                
-            cache[i][j] = count
-            return cache[i][j]
             
-            
-        return solve(0, 0)
+        return cache[0][0]
