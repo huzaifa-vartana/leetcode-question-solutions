4dda608cc71b9db7c1fbbb2fe6999219cc8c0620
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-26 01:43:23 +0500

    Time: 79 ms (55.56%), Space: 14.6 MB (49.26%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -3,18 +3,20 @@ class Solution:
         
         m, n, o = len(s1), len(s2), len(s3)
         
-        @lru_cache(None)
+        cache = [[-1 for _ in range(n+ 1)] for _ in range(m + 1)]
         def solve(i, j, k):
             if k >= o: 
                 return True if i >= m and j >= n else False
             if i >= m and j >= n: return False
+            if cache[i][j] != -1: return cache[i][j]
             
             
             if i < m and s1[i] == s3[k] and solve(i+1, j, k+1):
                 return True
             if j < n and s2[j] == s3[k] and solve(i, j+1, k+1):
                 return True
-                
+               
+            cache[i][j] = False
             return False
             
         
