d8eaf20d7d917bfeaca7fdbe89637da554d05ee2
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-25 22:48:45 +0500

    Time: 3080 ms (5.17%), Space: 44.9 MB (28.45%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
@@ -3,29 +3,24 @@ class Solution:
         n1 = len(nums1)
         n2 = len(nums2)
 
-        # cache = [[[-1 for _ in range(2)] for _ in range(n2+1)] for _ in range(n1+1)]
-        cache = {}
-        def solve(i, j, flag):
-            state = (i, j, flag)
-            if i == n1 or j == n2:
-                # cache[i][j][flag] = float('-inf') if not flag else 0
-                cache[state] = float('-inf') if not flag else 0
-                # return cache[i][j][flag]
-                return cache[state]
-            # if cache[i][j][flag] != -1: return cache[i][j][flag]
-            if state in cache: return cache[state]
+        cache = [[[float('-inf') for _ in range(2)] for _ in range(n2+1)] for _ in range(n1+1)]
+        for j in range(n2+1):
+            cache[n1][j][True] = 0
+        for i in range(n1+1):
+            cache[i][n2][True] = 0
+                
+        for i in range(n1-1, -1, -1):
+            for j in range(n2-1, -1, -1):
+                for flag in [True, False]:
+                    
+                    a = cache[i+1][j][flag]
+                    b = cache[i][j+1][flag] 
+                    c = nums1[i] * nums2[j] + cache[i+1][j+1][True] 
+                    d = cache[i+1][j+1][flag] 
 
-            a = solve(i+1, j, flag)
-            b = solve(i, j+1, flag)
-            c = nums1[i] * nums2[j] + solve(i+1, j+1, True)
-            d = solve(i+1, j+1, flag)
+                    cache[i][j][flag] = max(a, b, c, d)
 
-            # cache[i][j][flag] = max(a, b, c, d)
-            # return cache[i][j][flag]
-            cache[state] = max(a, b, c, d)
-            return cache[state]
-            
         
         
         
-        return solve(0, 0, False)
+        return cache[0][0][False]
