86af6e877f5c357c2a886f7ea1bbb32425eb8ee6
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-26 01:41:09 +0500

    Time: 44 ms (93.35%), Space: 14.9 MB (39.12%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        
+        m, n, o = len(s1), len(s2), len(s3)
+        
+        @lru_cache(None)
+        def solve(i, j, k):
+            if k >= o: 
+                return True if i >= m and j >= n else False
+            if i >= m and j >= n: return False
+            
+            
+            if i < m and s1[i] == s3[k] and solve(i+1, j, k+1):
+                return True
+            if j < n and s2[j] == s3[k] and solve(i, j+1, k+1):
+                return True
+                
+            return False
+            
+        
+        return solve(0, 0, 0)
+            
