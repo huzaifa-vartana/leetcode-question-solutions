71dd8792d8c9b5bc481b40e678aa8970fa18f810
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-27 01:20:53 +0500

    Time: 111 ms (24.03%), Space: 13.9 MB (90.77%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -2,24 +2,21 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         
         m, n, o = len(s1), len(s2), len(s3)
-        
-        cache = [[-1 for _ in range(n+ 1)] for _ in range(m + 1)]
-        
-        def solve(i, j):
-            if i+j >= o: 
-                return True if i >= m and j >= n else False
-            if i >= m and j >= n: return False
-            if cache[i][j] != -1: return cache[i][j]
+        if n + m != o: return False
+        cache = [[False for _ in range(n + 1)] for _ in range(m + 1)]
+        cache[m][n] = True
+
+                    
+                    
+        for i in range(m, -1, -1):
+            for j in range(n, -1, -1):
+                
+                if i < m and s1[i] == s3[i+j] and cache[i+1][j]:
+                    cache[i][j] = True
+                if j < n and s2[j] == s3[i+j] and cache[i][j+1]:
+                    cache[i][j] = True
+
             
-            
-            if i < m and s1[i] == s3[i+j] and solve(i+1, j):
-                return True
-            if j < n and s2[j] == s3[i+j] and solve(i, j+1):
-                return True
-               
-            cache[i][j] = False
-            return False
-            
-        
-        return solve(0, 0)
+
+        return cache[0][0]
             
