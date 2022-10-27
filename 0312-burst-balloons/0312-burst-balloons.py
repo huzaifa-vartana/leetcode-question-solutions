a963e427bf43f79dc362c5b31e837d89dafac730
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-27 22:56:58 +0500

    Time: 14414 ms (6.71%), Space: 16.9 MB (68.64%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,24 +1,21 @@
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
         
-
+        
         nums = [1] + nums + [1]
         n = len(nums)
-        cache = [[0 for _ in range(n+1)] for _ in range(n+1)]
-        
-
-        for i in range(n-2, 0, -1):
+        cache = [[0 for i in range(n+1)]for j in range(n+1)]
+        for i in range(n-1, 0, -1):
             for j in range(i, n-1):
-
-
+                
                 maxi = float('-inf')
                 for k in range(i, j+1):
-
                     maxi = max(
                         maxi,
                         cache[i][k-1] + cache[k+1][j] + nums[i-1] * nums[k] * nums[j+1]
                     )
 
-                cache[i][j] = max(maxi, cache[i][j])
-        
+                cache[i][j] = maxi
+            
+            
         return cache[1][n-2]
