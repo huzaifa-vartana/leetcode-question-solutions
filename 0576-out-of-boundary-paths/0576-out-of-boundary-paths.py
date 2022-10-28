8b6380ac8bc37262a85de652ad4ac8b01b279189
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-29 00:49:19 +0500

    Time: 207 ms (72.35%), Space: 19.7 MB (36.28%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
@@ -1,28 +1,23 @@
 class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
         
+        
         MOD = 10 ** 9 + 7
-        def valid(x, y):
-            return x >= 0 and x < m and y >= 0 and y < n
 
+        # cache = [[0 for _ in range(n+ 1)] for _ in range(m + 1)]
+        def valid(x, y): return x >= 0 and x < m and y >= 0 and y < n
 
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
-
-        cache = [[[-1 for _ in range(maxMove+1)] for _ in range(n+1)] for _ in range(m+1)]
+        @lru_cache(None)
         def solve(i, j, moves):
-
             if not valid(i, j): return 1
-            if moves <= 0: return 0
-            if cache[i][j][moves] != -1: return cache[i][j][moves]
-
+            if moves >= maxMove: return 0
+            
             count = 0
-            for dx, dy in directions:
-                nr, nc = dx + i, dy + j
-                count += solve(nr, nc, moves - 1)
-
-            cache[i][j][moves] = count % MOD
-            return cache[i][j][moves] 
-
-
-        return solve(startRow, startColumn, maxMove)
+            for nr, nc in [(i, j+1), (i+1, j), (i-1, j), (i, j-1)]:
+                count += solve(nr, nc, moves + 1)
+            
+            
+            return count % MOD
+            
+            
+        return solve(startRow, startColumn, 0)
