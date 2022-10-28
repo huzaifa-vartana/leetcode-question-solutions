0fbda45bfd9149b231fbef00f7d60aebbbfbe4af
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-10-28 23:46:13 +0500

    Time: 3583 ms (5.64%), Space: 187.5 MB (5.64%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
@@ -1,26 +1,22 @@
 class Solution:
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
-        n1 = len(nums1)
-        n2 = len(nums2)
-
-        cache = [[[float('-inf') for _ in range(2)] for _ in range(n2+1)] for _ in range(n1+1)]
-        for j in range(n2+1):
-            cache[n1][j][True] = 0
-        for i in range(n1+1):
-            cache[i][n2][True] = 0
-                
-        for i in range(n1-1, -1, -1):
-            for j in range(n2-1, -1, -1):
-                for flag in [True, False]:
-                    
-                    a = cache[i+1][j][flag]
-                    b = cache[i][j+1][flag] 
-                    c = nums1[i] * nums2[j] + cache[i+1][j+1][True] 
-                    d = cache[i+1][j+1][flag] 
-
-                    cache[i][j][flag] = max(a, b, c, d)
-
+        
+        m, n = len(nums1), len(nums2)
+        
+        @lru_cache(None)
+        def solve(i = 0, j = 0, chosen = False):
+            
+            if i >= m or j >= n:
+                return float('-inf') if not chosen else 0
+            
+            a = solve(i+1, j, chosen)
+            b = solve(i, j+1, chosen)
+            # c = solve(i+1, j+1, chosen)
+            d = nums1[i] * nums2[j] + solve(i+1, j+1, True)
+            
+            return max(a, b, float('-inf'), d)
         
         
         
-        return cache[0][0][False]
+        
+        return solve()
