0c20dcfc8fd5c5b6b53c25020151396e2bdff484
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-11-02 00:42:27 +0500

    Time: 1572 ms (57.87%), Space: 17 MB (39.02%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -2,7 +2,7 @@ class Solution:
     def maximalSquare(self, grid: List[List[str]]) -> int:
         
         m, n = len(grid), len(grid[0])
-        cache = [[-1 for _ in range(n+1)] for _ in range(m+1)]
+        cache = [[0 for _ in range(n+1)] for _ in range(m+1)]
         # for j in range(n):
         #     cache[m-1][j] = int(grid[m-1][j])
         # for i in range(m):
@@ -10,23 +10,19 @@ class Solution:
         def valid(x, y): return x >= 0 and x < m and y >= 0 and y < n
         
         maxi = 0
-        def solve(i, j):
-            if not valid(i, j): return 0
-            if cache[i][j] != -1: return cache[i][j]
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                
+                d = cache[i+1][j]
+                r = cache[i][j+1]
+                dig = cache[i+1][j+1]
 
-            d = solve(i+1, j)
-            r = solve(i, j+1)
-            dig = solve(i+1, j+1)
-
-            if grid[i][j] == '1':
-                ans = 1 + min(r, d, dig)
-                nonlocal maxi
-                maxi = max(maxi, ans)
-                cache[i][j] = ans
-                return ans
-            else:
-                cache[i][j] = 0
-                return 0
+                if grid[i][j] == '1':
+                    ans = 1 + min(r, d, dig)
+                    maxi = max(maxi, ans)
+                    cache[i][j] = ans
+                else:
+                    cache[i][j] = 0
             
-        solve(0, 0)
+        
         return maxi ** 2
