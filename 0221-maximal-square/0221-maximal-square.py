c8d02b8a854b284f02ce2e4f096b5706e1fa5b3a
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-11-02 00:49:40 +0500

    Time: 2051 ms (23.18%), Space: 17 MB (39.02%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -2,12 +2,17 @@ class Solution:
     def maximalSquare(self, grid: List[List[str]]) -> int:
         
         m, n = len(grid), len(grid[0])
-        cache = [[0 for _ in range(n+1)] for _ in range(m+1)]
-        def valid(x, y): return x >= 0 and x < m and y >= 0 and y < n
-        
+        cache = [[0 for _ in range(n)] for _ in range(m)]
         maxi = 0
-        for i in range(m-1, -1, -1):
-            for j in range(n-1, -1, -1):
+        for j in range(n):
+            cache[m-1][j] = int(grid[m-1][j])
+            maxi = max(maxi, cache[m-1][j])
+        for i in range(m):
+            cache[i][n-1] = int(grid[i][n-1])
+            maxi = max(maxi, cache[i][n-1])
+        
+        for i in range(m-2, -1, -1):
+            for j in range(n-2, -1, -1):
                 
                 d = cache[i+1][j]
                 r = cache[i][j+1]
