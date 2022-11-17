0e8fd904ac688ad3b85a62a02e106054e749df77
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2022-11-17 23:01:25 +0500

    Time: 2241 ms (69.80%), Space: 39.3 MB (28.57%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.py
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.py
@@ -6,23 +6,22 @@ class Solution:
         nums2 = [-1] + nums2
         n = len(nums1)
         
-        cache = [[-1 for _ in range(3)] for _ in range(n+1)]
-        def solve(i, swapped):
-            if i >= n: return 0
-            if cache[i][swapped] != -1: return cache[i][swapped]
+        cache = [[0 for _ in range(3)] for _ in range(n+1)]
+        for i in range(n-1, 0, -1):
+            for swapped in [True, False]:
+            
+                prev_number1 = nums1[i-1]
+                prev_number2 = nums2[i-1]
 
-            prev_number1 = nums1[i-1]
-            prev_number2 = nums2[i-1]
+                if swapped: prev_number1, prev_number2 = prev_number2, prev_number1
 
-            if swapped: prev_number1, prev_number2 = prev_number2, prev_number1
-
-            mini = float('inf')
-            if nums1[i] > prev_number1 and nums2[i] > prev_number2: mini = min(mini, solve(i+1, False))
+                mini = float('inf')
+                if nums1[i] > prev_number1 and nums2[i] > prev_number2: mini = min(mini, cache[i+1][False])
 
 
-            if nums1[i] > prev_number2 and nums2[i] > prev_number1: mini = min(mini, 1 + solve(i+1, True))
+                if nums1[i] > prev_number2 and nums2[i] > prev_number1: mini = min(mini, 1 + cache[i+1][True])
 
-            cache[i][swapped] = mini
-            return mini
+                cache[i][swapped] = mini
+
         
-        return solve(1, False)
+        return cache[1][False]
