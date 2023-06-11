2c8dcd4bc3b11449e69319a5b4c0b021f10d3b10
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2023-06-11 17:06:56 +0500

    Time: 47 ms (60.13%), Space: 16.3 MB (53.44%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,19 +3,16 @@ class Solution:
 
 
         def prefix_exists(idx):
-            prefix = strs[0][:idx+1]
-            print(prefix)
             for char_idx in range(1, len(strs)):
-                if strs[char_idx][:idx+1] != prefix: return False
+                if idx >= len(strs[char_idx]) or strs[char_idx][idx] != strs[0][idx]: return False
 
-                
             return True
+
         longest = ""
 
         for idx in range(len(strs[0])):
-            prefix = strs[0][:idx+1]
             if prefix_exists(idx):
-                longest = max(longest, prefix, key=len)
+                longest += strs[0][idx]
             else:
                 break
 
