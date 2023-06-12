f07849eef24823b3de11d83322e2195e8889acc4
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2023-06-12 21:06:08 +0500

    Time: 70 ms (23.62%), Space: 16.8 MB (29.28%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
@@ -1,29 +1,17 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
 
-        # def move_to_next_day(curr, day):
-        #     start, end = curr + 1, len(days) - 1
-        #     res = curr + 1
-        #     while start <= end:
-        #         mid = (start + end) // 2
-        #         if days[mid] == days[curr] + day:
-        #             res = mid
-        #             start = mid + 1
-        #         elif days[mid] > days[curr] + day:
-        #             res = mid
-        #             end = mid - 1
-        #         else:
-        #             res = mid
-        #             start = mid + 1
-
-        #     return res
         def move_to_next_day(curr, day):
+            start, end = curr, len(days)
+            while start < end:
+                mid = (start + end) // 2
+                if days[mid] <= days[curr] + day:
+                    start = mid + 1
+                else:
+                    end = mid
+            return start
 
-            ptr = curr + 1
-            while ptr < len(days) and days[curr] + day >= days[ptr]:
-                ptr += 1
 
-            return ptr
 
         cache = [-1] * (len(days)+1)            
         def solve(idx):
