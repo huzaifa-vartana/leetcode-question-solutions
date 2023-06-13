894551bfd84d4163d06956d36f27375d6884c6a4
Huzaifa Malik <45717453+huzaifamalik47@users.noreply.github.com>
2023-06-13 20:10:14 +0500

    Time: 49 ms (61.12%), Space: 16.3 MB (44.06%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -7,17 +7,9 @@ class Solution:
                 if idx >= len(s):
                     res.append(".".join(curr))
                 return
-            if idx >= len(s):
-                return
-
-            # rules for IP-addr
-            # rule 1. Each integer is between 0 and 255 (inclusive)
-            # rule 2. Cannot have leading zeros.
 
             for num_idx in range(idx, len(s)):
                 num = s[idx:num_idx+1]
-
-                # print(num)
                 if (len(num) > 1 and num[0]) != '0' and int(num) in range(256):
                     solve(num_idx+1, curr + [num])
 
