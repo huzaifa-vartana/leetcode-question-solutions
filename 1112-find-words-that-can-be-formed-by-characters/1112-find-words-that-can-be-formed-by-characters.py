c017e3add4880f2db460c798ed4b180c97071f1f
Huzaifa Malik <huzaifa@vartana.com>
2024-09-12 22:18:05 -0400

    Time: 98 ms (82.68%), Space: 17.1 MB (62.4%) - LeetHub

--- 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.py
+++ 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.py
@@ -1,19 +1,21 @@
+from collections import defaultdict
+from typing import Counter, List
+
+
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
-        char_counter = [0] * 26
+        char_counter = defaultdict(int)
 
         words_formed_len = 0
         for char in chars:
-            char_counter[ord(char) - ord('a')] += 1
+            char_counter[char] += 1
 
         for word in words:
-            word_counter = [0] * 26
-            for char in word:
-                word_counter[ord(char) - ord('a')] += 1
-
+            word_counter = defaultdict(int)
             is_word_formed = True
-            for i in range(26):
-                if word_counter[i] > char_counter[i]:
+            for char in word:
+                word_counter[char] += 1
+                if word_counter[char] > char_counter[char]:
                     is_word_formed = False
                     break
 
@@ -21,4 +23,3 @@ class Solution:
                 words_formed_len += len(word)
 
         return words_formed_len
-
