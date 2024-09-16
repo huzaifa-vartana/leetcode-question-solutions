d0c8e54389646dca46445d2e2fe4860e7da6b80e
Huzaifa Malik <huzaifa@vartana.com>
2024-09-15 22:51:35 -0400

    Time: 44 ms (31.62%), Space: 16.6 MB (28.77%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -4,11 +4,32 @@ from typing import List
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
 
-        # nums.sort()
-        max_num = max(nums)
+        nums.sort()
+        max_num = nums[-1]
+        n = len(nums)
 
+        def binary_search(nums, target):
+            left, right = 0, len(nums) - 1
+            while left < right:
+                mid = (left + right) // 2
+                if nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid
+            return len(nums) - left
+
+        cache = {}
         for i in range(max_num + 1):
-            if i == sum(1 for num in nums if num >= i):
+
+            if i in cache:
+                if cache[i] == i:
+                    return i
+                continue
+
+            # find out how many elements are greater than or equal to i using binary search
+            count = binary_search(nums, i)
+            cache[i] = count
+            if count == i:
                 return i
 
         return -1
