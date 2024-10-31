e2a9473d0d65e1d9ea7fb64dd4e068c5d0174d8c
Huzaifa Malik <huzaifa@vartana.com>
2024-10-30 22:52:56 -0400

    Time: 3431 ms (75.89%), Space: 16.9 MB (15.96%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,30 +1,38 @@
+from typing import List
+
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
-        n, m = len(board), len(board[0])
-        def valid(x, y): return x >= 0 and x < n and y >= 0 and y < m
-        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-        len_ = len(word)
-        
 
-        def solve(i, j, idx):
-            if idx >= len_: return True
-            tmp = board[i][j]
-            board[i][j] = -1
-            
-            
-            for dx, dy in directions:
-                nr, nc = dx+i, dy+j
-                if not valid(nr, nc) or board[nr][nc] == -1: continue
-                if word[idx] == board[nr][nc] and solve(nr, nc, idx+1): return True
-            
-            board[i][j] = tmp
+        N, M = len(board), len(board[0])
+        WORD_LEN = len(word)
+
+        def helper(i, j, word_idx):
+            if word_idx >= WORD_LEN:
+                return True
+
+            for x, y in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+                i_, j_ = i + x, j + y
+
+                if i_ < 0 or i_ >= N or j_ < 0 or j_ >= M:
+                    continue
+                if board[i_][j_] == "X":
+                    continue
+                if word[word_idx] != board[i_][j_]:
+                    continue
+
+                tmp = board[i][j]
+                board[i][j] = "X"
+                if helper(i_, j_, word_idx + 1):
+                    return True
+                board[i][j] = tmp
+
             return False
-        
-        
-        for i in range(n):
-            for j in range(m):
-                if board[i][j] == word[0] and solve(i, j, 1): return True
-                
-        
+
+        for i in range(N):
+            for j in range(M):
+                if board[i][j] == word[0] and helper(i, j, 1):
+                    return True
+
         return False
+
