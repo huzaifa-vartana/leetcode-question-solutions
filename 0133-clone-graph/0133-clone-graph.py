25798c734f8dc0bd87e0bd3377eaa71dd0c3e28a
Huzaifa Malik <huzaifa@vartana.com>
2024-11-08 18:18:52 -0500

    Time: 37 ms (77.32%), Space: 17.1 MB (20.42%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -11,27 +11,17 @@ class Solution:
     def __init__(self):
         self.original_to_clone_mapping = {}
 
-    def dfs(self, node):
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node: return
+        if node.val in self.original_to_clone_mapping: return self.original_to_clone_mapping[node.val]
             
         self.original_to_clone_mapping[node.val] = Node(node.val)
+
         for neighbor in node.neighbors:
-            if neighbor.val not in self.original_to_clone_mapping:
-                self.dfs(neighbor)
-
-            self.original_to_clone_mapping[node.val].neighbors.append(self.original_to_clone_mapping[neighbor.val])
-
-
-
-
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node: return None
-        self.dfs(node)
+            self.original_to_clone_mapping[node.val].neighbors.append(self.cloneGraph(neighbor))
 
 
         return self.original_to_clone_mapping[node.val]
-
-
-
 
 
         
