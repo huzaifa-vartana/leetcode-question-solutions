b792db7e823de9cef71756f81bbddfbd5c39c526
Huzaifa Malik <huzaifa@vartana.com>
2024-11-11 18:35:19 -0500

    Time: 107 ms (46.71%), Space: 52.2 MB (12.47%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.py
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -3,22 +3,21 @@ class Solution:
         N, M = len(grid), len(grid[0])
         def valid(x, y): return x >= 0 and x < N and y >= 0 and y < M
 
-        seen = set()
-        def dfs(i, j, seen):
-            if seen is not None: seen.add((i, j))
+        count = 0
+        def dfs(i, j):
             grid[i][j] = 0
             for i_, j_ in [(i, j+1), (i+1, j), (i-1, j), (i, j-1)]:
                 if not valid(i_, j_) or grid[i_][j_] == 0: continue
-                dfs(i_, j_, seen)
+                dfs(i_, j_)
 
         for i in range(N):
             for j in range(M):
                 if i == 0 or i == N - 1 or j == 0 or j == M - 1: 
-                    if grid[i][j] == 1: dfs(i, j, None)
+                    if grid[i][j] == 1: dfs(i, j)
 
         for i in range(N):
             for j in range(M):
-                if grid[i][j] == 1: dfs(i, j, seen)
+                if grid[i][j] == 1: count += 1
 
 
-        return len(seen)
+        return count
