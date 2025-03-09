351971d66540b0882dbb930cdd1242e5d2db8df8
Huzaifa Malik <huzaifa@vartana.com>
2025-03-09 05:03:12 -0400

    Time: 0 ms (100%), Space: 18 MB (16.45%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,24 +1,15 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
+        to = {}
+        from_ = {}
 
-        p_to_s = {}
-        s_to_p = {}
+        words = s.split(" ")
+        if len(words) != len(pattern): return False
 
-        s_split = s.split(" ")
+        for p, word in zip(pattern, words):
+            if (p in to and to[p] != word) or (word in from_ and from_[word] != p): return False
+            to[p] = word
+            from_[word] = p
 
-        if len(s_split) != len(pattern): return False
-
-        for char, word in zip(pattern, s_split):
-
-            if char in p_to_s and p_to_s[char] != word:
-                return False
-            if word in s_to_p and s_to_p[word] != char:
-                return False
-
-            p_to_s[char] = word
-            s_to_p[word] = char
 
         return True
-
-        
-        
