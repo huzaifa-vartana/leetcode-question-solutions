96d3bd2012e94f0c71a7416f841a12d19dbc4ca8
Huzaifa Malik <huzaifa@vartana.com>
2025-03-10 04:39:28 -0400

    Time: 0 ms (100%), Space: 18.5 MB (9.03%) - LeetHub

--- 1547-destination-city/1547-destination-city.py
+++ 1547-destination-city/1547-destination-city.py
@@ -2,11 +2,13 @@ class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
         paths_dict = {}
         for p1, p2 in paths:
-            paths_dict[p1] = paths_dict.get(p1, 0) + 1
-            paths_dict[p2] = paths_dict.get(p2, 0)
+            paths_dict[p1] = p2
 
-        for node, edges in paths_dict.items():
-            if edges == 0: return node
+        def dfs(node):
+            if node not in paths_dict: return node
+            return dfs(paths_dict[node])
+
+        return dfs(paths[0][0])
         
         
 
