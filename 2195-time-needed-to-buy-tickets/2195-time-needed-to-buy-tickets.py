c3153cc820504c7888defa73756bc5a7d3679083
Huzaifa Malik <huzaifa@vartana.com>
2025-03-11 01:48:14 -0400

    Time: 0 ms (100%), Space: 17.6 MB (83.44%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.py
@@ -1,20 +1,14 @@
-from collections import deque
-from typing import List
-
-
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        q = deque(tickets)
-
         time = 0
-        while True:
-            p = q.popleft()
+        for idx, ticket in enumerate(tickets):
 
-            if k == 0 and p-1 == 0:
-                return time + 1
+            if idx == k:
+                time += ticket
+            elif idx > k:
+                time += min(ticket, tickets[k] - 1)
+            else:
+                time += min(ticket, tickets[k])
 
-            if p-1 > 0:
-                q.append(p-1)
 
-            k = len(q) - 1 if k == 0 else k - 1
-            time += 1
+        return time
