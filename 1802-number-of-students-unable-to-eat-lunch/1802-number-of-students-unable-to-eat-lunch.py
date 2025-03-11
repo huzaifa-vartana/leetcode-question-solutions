b705dc2e778b6298f9e589c973a2db289c39db80
Huzaifa Malik <huzaifa@vartana.com>
2025-03-10 22:49:34 -0400

    Time: 0 ms (100%), Space: 18 MB (19.7%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.py
@@ -1,25 +1,16 @@
-from collections import deque
-from typing import List
-
-
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        students_q = deque(students)
-        sandwiches_idx = 0
-        students_processed = 0
+        ones = students.count(1)
+        zeros = students.count(0)
 
-        while True:
-            if students_q[0] == sandwiches[sandwiches_idx]:
-                students_q.popleft()
-                sandwiches_idx += 1
-                students_processed = 0
+
+        for idx, sandwich in enumerate(sandwiches):
+            if sandwich == 0:
+                zeros -= 1
             else:
-                students_q.append(students_q.popleft())
-                students_processed += 1
-                if students_processed == len(students_q):
-                    break
-            
-            if sandwiches_idx == len(sandwiches):
-                break
+                ones -= 1
 
-        return len(students_q)
+            if zeros < 0 or ones < 0: 
+                return len(sandwiches) - idx
+        # print(sandwich, ones, zeros)
+        return 0
