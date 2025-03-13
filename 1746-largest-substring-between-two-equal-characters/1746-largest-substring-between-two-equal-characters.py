bb37d7ffecc983e1285b7b009d08b3551302db3c
Huzaifa Malik <huzaifa@vartana.com>
2025-03-12 21:45:00 -0400

    Time: 0 ms (100%), Space: 18 MB (10.41%) - LeetHub

--- 1746-largest-substring-between-two-equal-characters/1746-largest-substring-between-two-equal-characters.py
+++ 1746-largest-substring-between-two-equal-characters/1746-largest-substring-between-two-equal-characters.py
@@ -5,11 +5,11 @@ class Solution:
         res = -1
         for idx, char in enumerate(s):
             if char in ctr:
-                first_index, last_index = ctr[char]
-                ctr[char] = [min(first_index, idx), max(last_index, idx)]
-                res = max(res, ctr[char][1] - ctr[char][0] - 1)
+                first_index = ctr[char]
+                ctr[char] = min(first_index, idx)
+                res = max(res, idx - ctr[char] - 1)
             else:
-                ctr[char] = [idx, idx]
+                ctr[char] = idx
 
         
         
