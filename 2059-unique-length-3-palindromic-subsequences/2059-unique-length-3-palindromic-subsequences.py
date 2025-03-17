b7f29bcc874ebc974a68753f3bfaf54de8364978
Huzaifa Malik <huzaifa@vartana.com>
2025-03-17 04:32:44 -0400

    Time: 747 ms (33.28%), Space: 18.4 MB (28.84%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
@@ -1,15 +1,21 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
+        res = 0
+        for c in set(s):
+            left_index, right_index = self.find_first_and_last_occurrences(s, c)
 
-        N = len(s)
-        ans = 0
-        for c in range(26):
-            i, j = N, -1
-            for k in range(N):
-                if ord(s[k]) - ord('a') == c:
-                    i = min(i, k)
-                    j = max(j, k)
-            ans += len(set(s[i + 1: j]))
+            if left_index < right_index:
+                res += len(set(s[left_index+1: right_index]))
 
-        return ans
+        return res
+    
 
+    def find_first_and_last_occurrences(self, s, c):
+        left_index, right_index = -1, -1
+        for idx in range(len(s)):
+            if s[idx] == c:
+                left_index = min(left_index, idx) if left_index != -1 else idx
+                right_index = max(right_index, idx)
+
+        return [left_index, right_index]
+            
