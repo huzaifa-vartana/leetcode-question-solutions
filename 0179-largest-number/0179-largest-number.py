cf037967ed8c9554edea5c1d3c96d55d8ec9cd4e
Huzaifa Malik <huzaifa@vartana.com>
2025-03-18 19:19:01 -0400

    Time: 3 ms (55.28%), Space: 17.8 MB (60.15%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -5,6 +5,17 @@ class Solution:
 
    
 
-        nums.sort(key=lambda x: x*10, reverse=True)
+        nums.sort(key=cmp_to_key(lambda x, y: (y + x > x + y) - (y + x < x + y)))
+        if nums[0] == '0':
+            return '0'
 
         return str(int(''.join(nums)))
+
+
+
+
+        
+
+
+
+        
