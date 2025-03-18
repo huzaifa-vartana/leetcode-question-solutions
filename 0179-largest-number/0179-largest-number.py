4ad9e21633df423f0b9ff90e2f287d93be5d7c0e
Huzaifa Malik <huzaifa@vartana.com>
2025-03-18 19:23:53 -0400

    Time: 3 ms (55.28%), Space: 17.7 MB (60.15%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,15 +1,22 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
 
-        nums = list(map(lambda x: str(x), nums))
+        def comparator(s1, s2):
+            a=s1+s2
+            b=s2+s1
+            if a>b: return -1
+            elif a<b: return 1
+            else: return 0
 
-   
 
-        nums.sort(key=cmp_to_key(lambda x, y: (y + x > x + y) - (y + x < x + y)))
-        if nums[0] == '0':
-            return '0'
+        nums = list(map(str, nums))
 
-        return str(int(''.join(nums)))
+
+        nums.sort(key=cmp_to_key(comparator))
+        if nums[0] == '0': return '0'
+        return "".join(nums)
+        
+
 
 
 
