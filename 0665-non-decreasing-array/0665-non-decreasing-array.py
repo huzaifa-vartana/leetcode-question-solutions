e34360acc236a1975e8d12d30976a4627e3f603e
Huzaifa Malik <huzaifa@vartana.com>
2025-03-18 23:22:59 -0400

    Time: 22 ms (5.07%), Space: 18.6 MB (68.93%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.py
+++ 0665-non-decreasing-array/0665-non-decreasing-array.py
@@ -1,20 +1,13 @@
-from typing import List
-
-
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
         N = len(nums)
+        mods = 0
+        for idx in range(1, N):
+            if nums[idx-1] > nums[idx]:
+                if idx-2 in range(N) and nums[idx-2] > nums[idx]:
+                    nums[idx] = nums[idx-1]
+                else:
+                    nums[idx-1] = nums[idx]
+                mods += 1
 
-        swaps = 0
-        for i in range(N-1):
-            if nums[i] <= nums[i + 1]:
-                continue
-
-            if i == 0 or nums[i-1] <= nums[i+1]:
-                nums[i] = nums[i+1]
-            else:
-                nums[i+1] = nums[i]
-
-            swaps += 1
-
-        return swaps == 0 or swaps == 1
+        return mods <= 1
