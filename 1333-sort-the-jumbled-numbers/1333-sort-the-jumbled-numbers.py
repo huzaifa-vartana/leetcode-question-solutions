39b424c42c10979c9d2ac53978ad6fd10577ba04
Huzaifa Malik <huzaifa@vartana.com>
2025-04-06 07:35:53 -0400

    Time: 574 ms (61.68%), Space: 27.4 MB (21.56%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
@@ -3,12 +3,31 @@ class Solution:
 
         ans = []
         for i, num in enumerate(nums):
-            mapped_num = ""
-            num = str(num)
-            for n in num:
-                mapped_num += str(mapping[int(n)])
+            mapped_num = 0
+            if num == 0: mapped_num = mapping[0]
 
-            ans.append((int(mapped_num), i))
+            """
+            991
+            1
+            1
+
+            1
+            90
+
+            900+91
+
+            """
+
+            base = 1
+            while num > 0:
+                last_digit = num % 10
+                num //= 10
+                mapped_num += base * mapping[last_digit]
+                base *= 10
+            
+            print(mapped_num)
+
+            ans.append((mapped_num, i))
 
         ans.sort()
         return [nums[i] for (_, i) in ans]
