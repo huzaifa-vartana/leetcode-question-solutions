4ee8454bf33e4c5128e170816783b825358c4e36
Huzaifa Malik <huzaifa@vartana.com>
2025-04-06 18:54:56 -0400

    Time: 0 ms (100%), Space: 17.9 MB (90.14%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -3,14 +3,13 @@ class Solution:
         nums1_dict = {nums1[i]: i for i in range(len(nums1))}
         stack = []
         ans = [-1] * len(nums1)
-
-        for idx in range(len(nums2)-1, -1, -1):
+        for idx in range(len(nums2)):
             num = nums2[idx]
             while stack and num > stack[-1][1]:
-                stack.pop()
+                _, ele = stack.pop()
 
-            if stack and num in nums1_dict:
-                ans[nums1_dict[num]] = stack[-1][1]
+                if ele in nums1_dict:
+                    ans[nums1_dict[ele]] = num
 
             stack.append((idx, num))
 
