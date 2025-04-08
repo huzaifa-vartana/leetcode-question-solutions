0b047df79c7732dabd8a3b5478dddc09101ebcbb
Huzaifa Malik <huzaifa@vartana.com>
2025-04-08 11:11:30 -0400

    Time: 0 ms (100%), Space: 17.8 MB (71.97%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,22 +1,23 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        i = j = 0
-        count = 0  # how many from nums2 have been inserted
-        
-        while i < m + count and j < n:
-            if nums1[i] <= nums2[j]:
-                i += 1
-            else:
-                # Shift nums1 right from i to m+count-1
-                for k in range(m + count, i, -1):
-                    nums1[k] = nums1[k - 1]
-                nums1[i] = nums2[j]
-                i += 1
-                j += 1
-                count += 1
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i, j, k = m - 1, n - 1, len(nums1) - 1
 
-        # If any nums2 elements are left, append them
-        while j < n:
-            nums1[m + count] = nums2[j]
-            j += 1
-            count += 1
+        # i -> end of actual items in nums1
+        # j -> end of nums2
+        # k -> end of nums1
+        while i >= 0 and j >= 0:
+            if nums2[j] >= nums1[i]:
+                nums1[k] = nums2[j]
+                j -= 1
+            else:
+                nums1[k] = nums1[i]
+                i -= 1
+
+            k -= 1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
