b872f6253331075c768f4337954598d608bfca46
Huzaifa Malik <huzaifa@vartana.com>
2025-04-08 17:44:03 -0400

    Time: 3 ms (71.55%), Space: 25.8 MB (21.26%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -2,20 +2,24 @@ class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        nums = [1,2,3,4,5,6,7], k = 3
+        7654321
+        5671234
+        nums = [-1,-100,3,99], k = 2
+        99,3,-100,-1
+        3,99,-1,-100
         """
-        def rev(i, j):
-            while i < j:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-                j -=1
-
         N = len(nums)
-        k = k % N
-        nums.reverse()
-        rev(0,k-1)
-        rev(k, N-1)
-        
-        
+        k %= N
+        self.reverse_list(nums, 0, N - 1)
+        self.reverse_list(nums, k, N - 1)
+        self.reverse_list(nums, 0, k - 1)
 
 
+    def reverse_list(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end   -= 1
         
+        return nums
