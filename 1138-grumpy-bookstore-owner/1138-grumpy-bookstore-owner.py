1ca0940333db734dcfc56071aaee4a1d8f8341cf
Huzaifa Malik <huzaifa@vartana.com>
2025-04-12 15:57:13 -0400

    Time: 27 ms (14.91%), Space: 19.4 MB (79.1%) - LeetHub

---
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        N = len(customers)
+        total = sum(customer for customer, is_grumpy in zip(customers, grumpy) if not is_grumpy)
+        
+        l, ans, local_all, local_real = 0, 0, 0, 0
+        for r in range(N):
+            local_all += customers[r]
+            if grumpy[r] == 0:
+                local_real += customers[r]
+            
+            if r - l + 1 > minutes:
+                local_all -= customers[l]
+                if grumpy[l] == 0:
+                    local_real -= customers[l]
+                l += 1
+            if r - l + 1 == minutes:
+                ans = max(ans, local_all - local_real)
+        ans += total
+        return ans
+
+
+        
+
