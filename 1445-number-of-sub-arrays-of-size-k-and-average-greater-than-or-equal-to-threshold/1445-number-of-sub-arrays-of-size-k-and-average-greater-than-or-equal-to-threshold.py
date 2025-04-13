14682d721e7009e7bffd67eb85ced5f086737a13
Huzaifa Malik <huzaifa@vartana.com>
2025-04-13 15:13:38 -0400

    Time: 63 ms (18.25%), Space: 28.1 MB (39.98%) - LeetHub

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,16 +1,21 @@
 class Solution:
     def numOfSubarrays(self, nums: List[int], k: int, threshold: int) -> int:
-        N = len(nums)
-        l, ans, summ, = 0, 0, 0
+        """
+        arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4
+        """
+        N, l, ans, summ = len(nums), 0, 0, 0
 
         for r in range(N):
+
             summ += nums[r]
 
+            # check and handle in-valid window of size K
             if r - l + 1 > k:
                 summ -= nums[l]
                 l += 1
 
-            if r - l + 1 == k and summ / k >= threshold: 
-                ans += 1
+            # this is valid window
+            if r - l + 1 == k and summ / k >= threshold: ans += 1
+
 
         return ans
