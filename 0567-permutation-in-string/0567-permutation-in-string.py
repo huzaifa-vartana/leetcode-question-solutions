be08a961fbdaf22bd504d38d632befa0f416e5bf
Huzaifa Malik <huzaifa@vartana.com>
2025-04-15 11:38:17 -0400

    Time: 25 ms (49.45%), Space: 17.9 MB (40.28%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,21 +3,16 @@ class Solution:
         M, N = len(s1), len(s2)
         if M > N:
             return False
-        count1 = {}
-        count2 = {}
-        for r in range(M): count1[s1[r]] = count1.get(s1[r], 0) + 1
+        count = Counter(s1)
         
         l = 0
         for r in range(N):
-            count2[s2[r]] = count2.get(s2[r], 0) + 1
-            
-            if r - l + 1 > M:
-                count2[s2[l]] -= 1
-                if count2[s2[l]] == 0:
-                    del count2[s2[l]]
+            count[s2[r]] -= 1
+            while count[s2[r]] < 0:
+                count[s2[l]] += 1
                 l += 1
-            if count1 == count2:
+            
+            if r - l + 1 == M:
                 return True
-
         
         return False
