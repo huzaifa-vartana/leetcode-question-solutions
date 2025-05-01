6cf1966ed03788c701fbbbd1a87a0c1a193a38f4
Huzaifa Malik <huzaifa@vartana.com>
2025-04-30 21:14:41 -0400

    Time: 183 ms (25.35%), Space: 18 MB (86.88%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,16 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        seen = defaultdict(int)
-        N, l, ans = len(s), 0, 0
-        for r in range(N):
-            r_char = s[r]
 
-            seen[r_char] += 1
+        # dynamic sliding window, hashmap
+        N = len(s)
+        max_length, count = 0, defaultdict(int)
+        left, right = 0, 0
 
-            if r - l + 1 - max(seen.values()) > k:
-                seen[s[l]] -= 1
-                l += 1
-            
-            ans = max(ans, r - l + 1)
-            
-        return ans
+        while right < N:
+
+            count[s[right]] += 1
+            while (right - left + 1) - max(count.values()) > k:
+                count[s[left]] -= 1
+                left += 1
+
+            max_length = max(max_length, right - left + 1)
+            right += 1
+
+        return max_length
+        
