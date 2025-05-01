b3c906e43b69ddf396a59b839772744a31ab1112
Huzaifa Malik <huzaifa@vartana.com>
2025-04-30 20:46:36 -0400

    Time: 63 ms (18.13%), Space: 28.2 MB (12.87%) - LeetHub

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,21 +1,24 @@
 class Solution:
     def numOfSubarrays(self, nums: List[int], k: int, threshold: int) -> int:
-        """
-        arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4
-        """
-        N, l, ans, summ = len(nums), 0, 0, 0
 
-        for r in range(N):
+        N = len(nums)
+        left = 0
+        count = 0
+        subarray_sum = 0
 
-            summ += nums[r]
+        for right in range(N):
+            subarray_sum += nums[right]
 
-            # check and handle in-valid window of size K
-            if r - l + 1 > k:
-                summ -= nums[l]
-                l += 1
-
-            # this is valid window
-            if r - l + 1 == k and summ / k >= threshold: ans += 1
+            if right - left + 1 > k:
+                subarray_sum -= nums[left]
+                left += 1
 
 
-        return ans
+            if right - left + 1 == k and subarray_sum / k >= threshold:
+                count += 1
+
+
+
+
+        return count
+        
