ce5ed83244c21b9bce242cbdd4d74d00413e46ac
Huzaifa Malik <huzaifa@vartana.com>
2025-05-02 00:17:42 -0400

    Time: 111 ms (43.13%), Space: 18.5 MB (17%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,26 +2,26 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         N, M = len(s), len(t)
 
-        t_freq_map = Counter(t)
-        s_freq_map = {}
+        freq_map = {}
+        for c in t: freq_map[c] = freq_map.get(c, 0) + 1
 
         left = 0
         min_string = ""
         min_string_len = float('inf')
         count = 0
+        lenn = len(freq_map)
 
         for right in range(N):
-            s_freq_map[s[right]] = s_freq_map.get(s[right], 0) + 1
-            if s_freq_map[s[right]] == t_freq_map[s[right]]: count += 1
+            freq_map[s[right]] = freq_map.get(s[right], 0) - 1
+            if freq_map[s[right]] == 0: count += 1
 
-            while count == len(t_freq_map):
+            while count == lenn:
                 if right - left + 1 < min_string_len:
                     min_string_len = right - left + 1
                     min_string = s[left:right+1]
 
-                s_freq_map[s[left]] -= 1
-                if s_freq_map[s[left]] < t_freq_map[s[left]]: count -= 1
-                if s_freq_map[s[left]] == 0: del s_freq_map[s[left]]
+                freq_map[s[left]] += 1
+                if freq_map[s[left]] > 0: count -= 1
                 left += 1
 
 
