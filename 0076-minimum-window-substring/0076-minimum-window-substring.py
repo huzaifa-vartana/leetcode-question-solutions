17f497e8a6b26c67ecb08b3f1659d9d43048abc4
Huzaifa Malik <huzaifa@vartana.com>
2025-05-02 00:13:41 -0400

    Time: 137 ms (31.32%), Space: 18.4 MB (17%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -8,23 +8,19 @@ class Solution:
         left = 0
         min_string = ""
         min_string_len = float('inf')
+        count = 0
 
-        def is_valid():
-            for k, v in t_freq_map.items():
-                if s_freq_map.get(k, 0) < v:
-                    return False
-            return True
-
-        # O (n) * 26
         for right in range(N):
             s_freq_map[s[right]] = s_freq_map.get(s[right], 0) + 1
+            if s_freq_map[s[right]] == t_freq_map[s[right]]: count += 1
 
-            while is_valid():
+            while count == len(t_freq_map):
                 if right - left + 1 < min_string_len:
                     min_string_len = right - left + 1
                     min_string = s[left:right+1]
 
                 s_freq_map[s[left]] -= 1
+                if s_freq_map[s[left]] < t_freq_map[s[left]]: count -= 1
                 if s_freq_map[s[left]] == 0: del s_freq_map[s[left]]
                 left += 1
 
