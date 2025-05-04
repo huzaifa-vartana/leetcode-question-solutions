8088f85290cc7ba95ddbf574e7ffccc70ae99105
Huzaifa Malik <huzaifa@vartana.com>
2025-05-03 20:04:42 -0400

    Time: 0 ms (100%), Space: 17.8 MB (62.12%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,20 +1,24 @@
 from typing import List
 
-
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         n = len(nums)
         l, r = 0, n - 1
-        while l < r:
-            m = (l+r) // 2
-            if (m == 0 and nums[m] > nums[m+1]) or (m == n - 1 and nums[m] > nums[m-1]):
-                return m
-            if m+1 in range(n) and nums[m] > nums[m+1] and m-1 in range(n) and nums[m] > nums[m-1]:
+        
+        while l <= r:
+            m = (l + r) // 2
+
+            # Check if middle is a peak
+            left = nums[m - 1] if m > 0 else float('-inf')
+            right = nums[m + 1] if m < n - 1 else float('-inf')
+            
+            if nums[m] > left and nums[m] > right:
                 return m
 
-            if m+1 in range(n) and nums[m] < nums[m+1]:
+            # Move to the side with the higher neighbor
+            if nums[m] < right:
                 l = m + 1
             else:
                 r = m - 1
 
-        return l
+        return -1  # This line is never reached if there's always at least one peak
