4ce82cc8bedf5a3e1882317adb3c19101c268851
Huzaifa Malik <huzaifa@vartana.com>
2025-05-04 00:53:08 -0400

    Time: 231 ms (23.66%), Space: 22.3 MB (18.92%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,30 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        n = len(weights)
+        left, right = 1, sum(weights)
+        ans = right
+
+        while left <= right:
+            capacity = (left+right) // 2
+            possible = self.is_pos(weights, days, capacity)
+
+            if possible:
+                ans = capacity
+                right = capacity - 1
+            else:
+                left = capacity + 1
+
+        return ans
+    
+    def is_pos(self, weights, days, capacity):
+        n = len(weights)
+        count = 1
+        total = 0
+        for i in range(n):
+            total += weights[i]
+            if weights[i] > capacity:
+                return False
+            if total > capacity:
+                count += 1
+                total = weights[i]
+        return count <= days
